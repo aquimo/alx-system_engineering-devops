b7f6ea61b526f8dceb5473412152f9a75113f338
aquimo <aquimoadriano@gmail.com>
2024-06-18 14:26:27 +0200

    file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,14 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  content => template('my_module/wp-settings.php.erb'),
 }
+
+exec { 'fix-wordpress':
+  command     => '/usr/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path        => ['/usr/bin', '/bin'],
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
+}
+
