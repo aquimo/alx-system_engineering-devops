c9cbc36bbcec037687f107dd422d561330ad3e25
aquimo <aquimoadriano@gmail.com>
2024-09-27 21:01:48 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# automated puppet fix (to find out why Apache is returning a 500 error)
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => template('my_module/wp-settings.php.erb'),
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
-
-exec { 'fix-wordpress':
-  command     => '/usr/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => ['/usr/bin', '/bin'],
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
-}
-
